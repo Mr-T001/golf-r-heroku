341ae63df177d69e34247e49ec509e7d4c2f4a39
Mr-T001 <56784576+Mr-T001@users.noreply.github.com>
2023-01-03 12:12:19 +0000

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,49 @@
+# Import libraries
+library(shiny)
+library(shinythemes)
+library(data.table)
+library(RCurl)
+library(randomForest)
+
+# Save model to RDS file
+# saveRDS(model, "model.rds")
+
+# Read in the RF model
+#model <- readRDS("model.rds")
+
+####################################
+# User interface                   #
+####################################
+
+fluidPage(theme = shinytheme("united"),
+                
+                # Page header
+                headerPanel('Play Golf?'),
+                
+                # Input values
+                sidebarPanel(
+                  HTML("<h3>Input parameters</h3>"),
+                  
+                  selectInput("outlook", label = "Outlook:", 
+                              choices = list("Sunny" = "sunny", "Overcast" = "overcast", "Rainy" = "rainy"), 
+                              selected = "Rainy"),
+                  sliderInput("temperature", "Temperature:",
+                              min = 64, max = 86,
+                              value = 70),
+                  sliderInput("humidity", "Humidity:",
+                              min = 65, max = 96,
+                              value = 90),
+                  selectInput("windy", label = "Windy:", 
+                              choices = list("Yes" = "TRUE", "No" = "FALSE"), 
+                              selected = "TRUE"),
+                  
+                  actionButton("submitbutton", "Submit", class = "btn btn-primary")
+                ),
+                
+                mainPanel(
+                  tags$label(h3('Status/Output')), # Status/Output Text Box
+                  verbatimTextOutput('contents'),
+                  tableOutput('tabledata') # Prediction results table
+                  
+                )
+)
